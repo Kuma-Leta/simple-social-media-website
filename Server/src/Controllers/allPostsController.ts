b06eb105d6eb1f69f5f5bd8610416e693c1a695e
kuma <kumaleta2021@gmail.com>
2024-06-21 13:09:57 +0300

    get all post implemented

--- Server/src/Controllers/allPostsController.ts
+++ Server/src/Controllers/allPostsController.ts
@@ -1,2 +1,14 @@
-import { Request, Response } from "express";
-export const getAllPosts = (req: Request, res: Response) => {};
+import { NextFunction, Request, Response } from "express";
+import { postModel } from "../models/postModel";
+import AppError from "../globalErrorHandling/appError";
+export const getAllPosts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const posts = await postModel.find();
+  if (posts.length === 0) {
+    next(new AppError("no posts found", 404));
+  }
+  res.status(200).json({ message: "success", posts });
+};
