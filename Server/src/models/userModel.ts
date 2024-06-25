f3d8834583680ac9d1b7b22e4b13b2ac1d081109
kuma <kumaleta2021@gmail.com>
2024-06-25 14:53:09 +0300

    user model enhanced to include fields for password reset token and time for when password expires

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 import { Schema, model } from "mongoose";
 
 interface User {
@@ -5,7 +7,11 @@ interface User {
   email: string;
   password: string;
   confirmPassword: string;
+  passwordResetToken?: string;
+  passwordResetExpires?: Date;
+  createPasswordResetToken(): string;
 }
+
 const userSchema = new Schema<User>(
   {
     name: {
@@ -21,9 +27,30 @@ const userSchema = new Schema<User>(
       type: String,
       required: true,
     },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = new Date(Date.now() + 60 * 60 * 1000);
+  return resetToken;
+};
+
 export const userModel = model("user", userSchema);
