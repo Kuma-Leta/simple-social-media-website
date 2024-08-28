6bb95002338885641d954ec944a1c02c6849c181
kuma <kumaleta2021@gmail.com>
2024-08-28 15:01:08 +0300

    server side of like functionality implemented

---
+++ Server/src/Controllers/likeController.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Response } from "express";
+import { AuthenticatedRequest } from "../middleware/authenticationMiddleware";
+import { postModel } from "../models/postModel";
+import { likeModel } from "../models/likeModel";
+
+const addLikeForPost = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { postId } = req.body;
+  const post = await postModel.findById(postId);
+  let previousLike = Number(post?.likes) || 0;
+  const like = await likeModel.findOne({ postId, user: req.user });
+  if (like) {
+    previousLike -= 1;
+    await likeModel.findOneAndDelete({ postId, user: req.user });
+  } else {
+    previousLike += 1;
+    await likeModel.create({
+      postId,
+      user: req.user,
+    });
+  }
+  const updatedLike = previousLike;
+  const updatedPost = await postModel.findByIdAndUpdate(
+    postId,
+    {
+      likes: updatedLike,
+    },
+    { new: true }
+  );
+
+  res.status(200).json({ status: "success", updatedPost });
+};
+export default addLikeForPost;
