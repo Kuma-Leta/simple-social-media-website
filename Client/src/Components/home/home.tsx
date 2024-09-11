5cb4598b65119bc9d206b95d6ef135c470b0a757
kuma <kumaleta2021@gmail.com>
2024-09-11 12:49:02 +0300

    loading... functionality added for better interface

--- Client/src/Components/home/home.tsx
+++ Client/src/Components/home/home.tsx
@@ -5,6 +5,8 @@ import axios from "../../axiosConfig";
 import Rating from "./handleRating";
 import CommentPopup from "./comment";
 import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:5000/api");
 interface Comment {
   _id: string;
   user: string;
@@ -23,7 +25,7 @@ export interface post {
   likes: number;
   comments: Array<Comment>;
 }
-
+const useReducer = (state, action) => {};
 const HomePage: React.FC = () => {
   const [userQuery, setuserQuery] = useState("");
   const [posts, setPosts] = useState<post[]>([]);
@@ -31,7 +33,23 @@ const HomePage: React.FC = () => {
   const [user, setUser] = useState("");
   const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isSystemOption, setSystemOption] = useState(true);
+  const [isPostAvailable, setIsPostAvailable] = useState(false);
   const navigate = useNavigate();
+  useEffect(() => {
+    socket.on("newLike", (data) => {
+      alert(data.message);
+    });
+
+    socket.on("newComment", (data) => {
+      alert(data.message);
+    });
+
+    return () => {
+      socket.off("newLike");
+      socket.off("newComment");
+    };
+  }, []);
   useEffect(() => {
     const getAllPosts = async () => {
       try {
@@ -44,18 +62,23 @@ const HomePage: React.FC = () => {
           "http://localhost:5000/api/getAllposts"
         );
         setPosts(allPosts.data.posts);
+        setIsPostAvailable(true);
       } catch (error: any) {
         console.log(error.message);
       }
     };
     getAllPosts();
   }, []);
+
   const handleLogout = () => {
-    try {
-      localStorage.removeItem("authToken");
-      navigate("/login");
-    } catch (error) {
-      console.log(error);
+    const userConfirmed = window.confirm("are you sure you want to logout ?");
+    if (userConfirmed) {
+      try {
+        localStorage.removeItem("authToken");
+        navigate("/login");
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
   const handleKeyDown = async (
@@ -97,13 +120,15 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className={`homeContainer ${isPopupOpen ? "blur" : ""}`}>
+    <div className="homeContainer ">
       <header>
         <h1>
           <Link to={"/home"}>Blog Post.</Link>
         </h1>
         <div className="profileAndSearchContainer">
-          <Link to={"/profile"}>your profile 👮‍♂️ :{user}</Link>
+          <Link to={"/profile"}>
+            <i class="fas fa-user"></i> profile :{user}
+          </Link>
           <div>
             <input
               type="text"
@@ -111,99 +136,163 @@ const HomePage: React.FC = () => {
               onChange={(e) => setuserQuery(e.target.value)}
               onKeyDown={handleKeyDown}
             />
+            <button className="notifications">
+              <i className="fas fa-bell"></i>
+              <span>10</span>
+            </button>
           </div>
         </div>
       </header>
-      <div className="postAndSystemOptionsContainer">
-        <div className="sytemOptions">
-          <button>categories</button>
-          <button>View your Profile</button>
-          <button>Create your Post</button>
-          <button onClick={handleLogout}>logout</button>
-          <button>Notifications</button>
-          <button>your posts</button>
-          <button>videos</button>
-        </div>
-        <div className="postsContainer">
-          {posts.map((post: post) => (
-            <div className="post" key={post._id}>
-              <div className="eachPost">
-                <div className="postDropdown">
-                  <button>...</button>
-                </div>
-                <p className="author">Author :{post.author}</p>
-                <p className="textContent"> {post.textContent}</p>
-                {post.imageContent && (
-                  <div>
-                    <img
-                      src={`http://localhost:5000/${post.imageContent}`}
-                      alt="Post"
-                    />
-                  </div>
-                )}
-                {post.videoContent && (
-                  <div>
-                    <video
-                      src={`http://localhost:5000/${post.videoContent}`}
-                      controls
-                    />
-                  </div>
-                )}
-                <div className="LCcontainer">
-                  <div className="likeAndComment">
-                    <div className="likeRatingComment">
-                      <p>rating :{post.rating.toFixed(1)}</p>
-                      <button onClick={() => handleLikes(post._id)}>
-                        🤍 {post.likes}
-                      </button>
-                      <button onClick={() => handleCommentClick(post._id)}>
-                        🖊 comment
-                      </button>
-                    </div>
-
-                    <Rating
-                      postId={post._id}
-                      initialRating={post.rating}
-                      user={post.user}
-                      updateRating={updateRating}
-                    />
-                  </div>
-                </div>
-                <form className="addComment" action="">
-                  <input
-                    className="addComment"
-                    type="text"
-                    placeholder="leave a comment"
-                  />
-                  <input type="submit" value={"add"} />
-                </form>
-              </div>
-              <div className="commentsContainer">
-                <h3>Comments</h3>
-                {post.comments.length === 0 && (
-                  <p className="noComments">
-                    No Comments yet be the first to comment
-                  </p>
-                )}
-                <ul>
-                  {post.comments.map((comment) => (
-                    <li key={comment._id} className="comment">
-                      <p>{comment.comment}</p>
-                      <p>
-                        <small>by {comment.user}</small>
-                      </p>
-                      <p>
-                        <small>{new Date(comment.date).toLocaleString()}</small>
-                      </p>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+      {isPostAvailable ? (
+        <div className="postAndSystemOptionsContainer">
+          <div className="menuAndSystemOption">
+            <div className="menu">
+              <button
+                onClick={() => setSystemOption(!isSystemOption)}
+                title="menu"
+                className=""
+              >
+                <i className="fas fa-bars"></i>
+              </button>
             </div>
-          ))}
-          {posts.length === 0 && <div className="eachPost">No posts Found</div>}
+            {isSystemOption && (
+              <div className="sytemOptions">
+                <button className="categoriesContainer">
+                  <p>categories</p>
+                  <div className="categories">
+                    {["technology", "Arts", "politics", "sports"].map(
+                      (category, index) => (
+                        <ul>
+                          <li key={index}>{category}</li>
+                        </ul>
+                      )
+                    )}
+                  </div>
+                </button>
+                <button
+                  title="click to view your profile"
+                  onClick={() => navigate("/profile")}
+                >
+                  View your Profile
+                </button>
+                <button
+                  title="click to create your post"
+                  onClick={() => navigate("/profile/addPost")}
+                >
+                  Create your Post
+                </button>
+                <button title="click to logout" onClick={handleLogout}>
+                  logout
+                </button>
+                <button
+                  title="click to view your notifications"
+                  className="notifications"
+                >
+                  Notifications<i className="fas fa-bell"></i>{" "}
+                  <span className="notSize"> 25 </span>
+                </button>
+                <button
+                  title="click to view your posts"
+                  onClick={() => navigate("/profile")}
+                >
+                  your posts
+                </button>
+                <button title="click to view  videos">
+                  settings <i class="fas fa-cog"></i>
+                </button>
+              </div>
+            )}
+          </div>
+          <div className="postsContainer">
+            {posts.map((post: post) => (
+              <div className="post" key={post._id}>
+                <div className="eachPost">
+                  <div className="postDropdown">
+                    <button>...</button>
+                  </div>
+                  <p className="author">Author :{post.author}</p>
+                  <p className="textContent"> {post.textContent}</p>
+                  {post.imageContent && (
+                    <div>
+                      <img
+                        src={`http://localhost:5000/${post.imageContent}`}
+                        alt="Post"
+                      />
+                    </div>
+                  )}
+                  {post.videoContent && (
+                    <div>
+                      <video
+                        src={`http://localhost:5000/${post.videoContent}`}
+                        controls
+                      />
+                    </div>
+                  )}
+                  <div className="LCcontainer">
+                    <div className="likeAndComment">
+                      <div className="likeRatingComment">
+                        <p>
+                          <i class="fas fa-star"></i>:{post.rating.toFixed(1)}
+                        </p>
+                        <button onClick={() => handleLikes(post._id)}>
+                          <i class="fas fa-thumbs-up"></i>
+                          {post.likes}
+                        </button>
+                        <button onClick={() => handleCommentClick(post._id)}>
+                          <i class="fas fa-comment"></i>
+                          comment
+                        </button>
+                        <Rating
+                          postId={post._id}
+                          initialRating={post.rating}
+                          user={post.user}
+                          updateRating={updateRating}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="commentsContainer">
+                  <h3>Comments</h3>
+                  {post.comments.length === 0 && (
+                    <p className="noComments">
+                      No Comments yet be the first to comment
+                    </p>
+                  )}
+                  <ul>
+                    {post.comments.map((comment) => (
+                      <li key={comment._id} className="comment">
+                        <p>{comment.comment}</p>
+                        <p>
+                          <small>by {comment.user}</small>
+                        </p>
+                        <p>
+                          <small>
+                            {new Date(comment.date).toLocaleString()}
+                          </small>
+                        </p>
+                      </li>
+                    ))}
+                  </ul>
+                  <form className="addComment" action="">
+                    <input
+                      className="addComment"
+                      type="text"
+                      placeholder="leave a comment"
+                    />
+                    <input type="submit" value={"add"} />
+                  </form>
+                </div>
+              </div>
+            ))}
+            {posts.length === 0 && (
+              <div className="eachPost">No posts Found</div>
+            )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <p className="loading">loading...</p>
+      )}
 
       {selectedPostId && (
         <CommentPopup postId={selectedPostId} onClose={closePopup} />
