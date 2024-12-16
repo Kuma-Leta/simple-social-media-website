114c45b58e70ecdc34e8f61b0c1125f634b3c0e6
kuma <kumaleta2021@gmail.com>
2024-12-16 16:31:49 +0300

    make responsive commentList and posts

--- Client/src/Components/home/postList.tsx
+++ Client/src/Components/home/postList.tsx
@@ -3,21 +3,29 @@ import CommentsList from "./commentsList";
 import Posts from "./posts";
 import { selectPosts } from "../../store/postSlice";
 import { Post } from "../../store/postSlice";
+
 const PostList: React.FC = () => {
   const posts = useSelector(selectPosts);
+
   return (
-    <div className="postsContainer bg-white">
+    <div className="bg-white p-4 w-full h-full">
       {posts && posts.length > 0 ? (
         posts.map((post: Post) => (
-          <div className="post font-serif" key={post._id}>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-[70%_30%] gap-4 bg-gray-100 p-4 rounded-lg shadow-md"
+            key={post._id}
+          >
             <Posts post={post} />
             <CommentsList post={post} />
           </div>
         ))
       ) : (
-        <div className="eachPost">No posts Found</div>
+        <div className="eachPost text-center text-gray-500 font-semibold">
+          No posts Found
+        </div>
       )}
     </div>
   );
 };
+
 export default PostList;
