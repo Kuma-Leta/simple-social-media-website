fdd8b0aac4194fc8c1156bf70da74b46f21af0c9
kuma <kumaleta2021@gmail.com>
2025-01-01 19:59:36 +0300

    make home responsive

--- Client/src/Components/home/home.tsx
+++ Client/src/Components/home/home.tsx
@@ -5,32 +5,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../store";
 import Header from "./header";
 import SystemOption from "./systemOption";
-import { getUser } from "../../store/userSlice";
+import { getUser, selectUsername } from "../../store/userSlice";
 import PostList from "./postList";
 import socket from "./connectSocket";
+import { Helmet } from "react-helmet";
 const HomePage: React.FC = () => {
   const Dispatch = useDispatch<AppDispatch>();
   const isLoading = useSelector(selectIsLoading);
-
+  const user = useSelector(selectUsername);
   useEffect(() => {
     async function getUser() {
-      // socket.emit("userOnline", user?.User._id);
-
+      socket.emit("userOnline", user._id);
       return () => {
-        socket.off("newLike");
-        socket.off("newComment");
+        socket.off("userOnline");
       };
     }
     getUser();
-  }, []);
+  }, [user]);
+
   useEffect(() => {
     const getAllPosts = async () => {
       try {
-        document.title = "Your Posts";
-
         Dispatch(getUser());
         Dispatch(fetchPosts());
-        // dispatch({ type: "SET_IS_POST_AVAILABLE", payload: true });
       } catch (error: any) {
         console.log(error);
       }
@@ -39,14 +36,24 @@ const HomePage: React.FC = () => {
   }, [Dispatch]);
 
   return (
-    <div className="homeContainer ">
+    <div className="homeContainer">
+      <Helmet>
+        <title>home</title>
+        <meta name="description" content="access the home page from here " />
+      </Helmet>
       <Header />
       {isLoading ? (
         <p className="loading">loading...</p>
       ) : (
-        <div className="postAndSystemOptionsContainer flex flex-col lg:flex-row lg:gap-6 px-4 lg:px-10 py-6 ">
-          <SystemOption />
-          <PostList />
+        <div className="postAndSystemOptionsContainer flex flex-col lg:flex-row gap-2 px-4 lg:px-10 py-6">
+          {/* Hide SystemOption on smaller screens */}
+          <div className="hidden lg:block lg:w-1/4">
+            <SystemOption />
+          </div>
+          {/* PostList should take the full width on smaller screens */}
+          <div className="w-full lg:w-3/4">
+            <PostList />
+          </div>
         </div>
       )}
     </div>
